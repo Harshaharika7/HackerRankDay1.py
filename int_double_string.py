56c4145c5c413e37a7a5e8c71cf809a43816d8a2
Harshaharika7 <138356696+Harshaharika7@users.noreply.github.com>
2025-05-15 22:18:41 +0530

    Create int_double_string.py

---
+++ int_double_string.py
@@ -0,0 +1,13 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# 1. Declare three new variables
+int = int(input())           # Read an integer input
+double= float(input())      # Read a float/double input
+str = input()             # Read a string input
+
+# 2. Perform the required operations and print results
+print(i + int)               # Print the sum of i and int_input
+print(f"{d + double:.1f}")   # Print the sum of d and double_input to 1 decimal
+print(s + str)           # Concatenate and print the strings
